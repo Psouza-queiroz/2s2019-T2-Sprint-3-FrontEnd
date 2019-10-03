f76564aa0d7511dac0ebd5d58f6c4551a6455571
Pedro Henrique Souza De Queiroz <38128442813@info.local>
2019-10-03 17:27:49 -0300

    up

---
+++ tarde/gufos-react/src/pages/Contato/Contato.js
@@ -0,0 +1,16 @@
+// import React,{ Component } from "react";
+
+
+// class Contato extends Component{
+
+
+
+//    render(){
+//        return(
+
+        
+//        )
+
+//    }
+// }
+// export default Contato;
---
+++ tarde/gufos-react/src/pages/Eventos/Eventos.js
@@ -0,0 +1,123 @@
+import React,{ Component } from "react";    
+
+import Rodape from '../../components/Rodape'
+
+import logo from '../../assets/img/icon-login.png'
+import Axios from "axios";
+import Titulo from "../../components/Titulo";
+
+class Eventos extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+         lista:[]   
+        }
+    }
+        componentDidMount(){
+            this.listarEventos();
+        }
+
+        listarEventos = () =>{
+            // event.preventDefault();
+            Axios.get('http://192.168.7.85:5000/api/eventos')
+            .then(response => {
+                this.setState({lista:response.data})             
+            })
+        }
+        CadastrarEventos(){
+          Axios.post('http://192.168.7.85:5000/api/categorias',{
+              titulo:this.state.Titulo,
+              dataEvento:this.state.dataEvento,
+              ativo:this.state.ativo,
+              idCategoriaNavigation:this.state.idCategoriaNavigation.nome
+          })
+            .then(data =>{
+                if (data.status === 200){
+                  console.log(data.data)
+                }else{
+
+                }
+            })
+        }       
+
+
+    render(){
+        return(
+            <div>
+  <header className="cabecalhoPrincipal">
+    <div className="container">
+      <img src={logo}/>
+
+      <nav className="cabecalhoPrincipal-nav">
+        Administrador
+      </nav>
+    </div>
+  </header>
+
+  <main className="conteudoPrincipal">
+    <section className="conteudoPrincipal-cadastro">
+      {/* <h1 className="conteudoPrincipal-cadastro-titulo">Eventos</h1> */}
+    <Titulo titulo="Eventos"/>
+   
+      <div className="container" id="conteudoPrincipal-lista">
+
+        <table id="tabela-lista">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Evento</th>
+              <th>Data</th>
+              <th>Acesso Livre</th>
+              <th>Tipo do Evento</th>
+            </tr>
+          </thead>
+
+          <tbody id="tabela-lista-corpo">
+              {this.state.lista.map(element => {
+                  return(
+                      <tr>
+                        <td>{element.idEvento}</td>
+                        <td>{element.titulo}</td>
+                        <td>{element.dataEvento}</td>
+                        <td>{(element.ativo) ? 'sim':'nao'}</td>
+                        <td>{element.idCategoriaNavigation.nome}</td>
+                      </tr>
+                  )
+              })}
+          </tbody>
+        </table>
+
+      </div>
+
+      <div className="container" id="conteudoPrincipal-cadastro">
+        <h2 className="conteudoPrincipal-cadastro-titulo">Cadastrar Evento</h2>
+        <div className="container">
+
+          <input type="text" id="evento__titulo" placeholder="título do evento"/>
+          <input type="text" id="evento__localizacao" placeholder="localização"/>
+          <input type="text" id="evento__data" placeholder="dd/MM/yyyy"/>
+          <select id="option__acessolivre">
+            <option value="1">Ativo</option>
+            <option value="0">Desativo</option>
+          </select>
+          <select id="option__tipoevento">
+            <option value="0" disabled>Categoria do Evento</option>
+          </select>
+          <textarea rows="3" cols="50" placeholder="descrição do evento" id="evento__descricao"></textarea>
+
+        </div>
+        <button className="conteudoPrincipal-btn conteudoPrincipal-btn-cadastro" value="submit">Cadastrar</button>
+      </div>
+    </section>
+  </main>
+
+    <Rodape/>
+
+</div>
+
+
+            )
+    }
+}
+export default Eventos;
