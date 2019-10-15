f3f4da836e045a457e7d702afdbed088bb932524
Psouza-queiroz <pedrohsouzaqueiroz@gmsil.com>
2019-10-15 16:51:39 -0300

    up

--- Opflix/opflix/src/Pages/Login/Login.js
+++ Opflix/opflix/src/Pages/Login/Login.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import logo from "../../assets/img/logo_opflix.png"
-import '../Home/App.css';
+import preto from "../../assets/img/preto.png"
+import '../../assets/css/Login.css'
 
 
 import { Link } from "react-router-dom";
 function Login(){
     return(
         <div className="App">
-            
-            <Link to="/"><img src={logo} /></Link>
-                    </div>
-    )
+          <div className="imagem">
+              <img className="logo" src={logo} alt="" width="100%" height="100%"/>
+              <img src={preto}  width="100%" height="100%"/>
+            </div>
+            <div className="pequeRoda">
+
+              <h2>Escola Senai De Informatica</h2>
+              <p>senai@Informatica.com.br</p>
+              <p>Endereço:  Alameda Barão de Limeira, 539 - Santa Cecilia</p>
+              <p>Sistema Feito Por Pedro</p>
+              
+            </div>
+
+        </div>
+            )
 }
 export default Login; 
